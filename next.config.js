3af4a9bdd7ced419cf901279e8dbf4c066424ee9
nige00 <45668326+nige00@users.noreply.github.com>
2021-09-02 04:04:22 +0000

    modified next.config.js

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-// ここから
-  webpackDevMiddleware: config => {
-    config.watchOptions = {
-      poll: 800,
-      aggregateTimeout: 300,
-    }
-    return config
-  },
-// ここまで
-}
+};
